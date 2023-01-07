583875ba21ff530c7c2f07c88583de8d2b905602
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-01-07 02:45:11 +0100

    criei icones para o login

---
+++ public/javascripts/index.js
@@ -0,0 +1,6 @@
+const btnLogin = document.querySelector('#btnLogar');
+const btnRegistar = document.querySelector('#btnRegistar');
+
+btnLogin.addEventListener('click', () =>{ location.href = '/login'; });
+
+btnRegistar.addEventListener('click', () =>{ location.href = '/registar'; });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'SISCAF - Gestão do Centro' });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Inicio de Sessão' });
+});
+
+router.get('/registar', (req, res, next) => {
+  res.render('registar', { title: 'Registar Novo Usuário' });
 });
 
 module.exports = router;
