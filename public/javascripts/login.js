be353c7c90562831a008f8c5536dc675f0f4603b
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-01-09 00:43:42 +0100

    Layout de login

---
+++ public/javascripts/login.js
@@ -0,0 +1,7 @@
+const btnCancelar = document.querySelector('#btnCancelar');
+btnCancelar.addEventListener('click', () => {
+    document.querySelector('#userName').value = '';
+    document.querySelector('#senha').value = '';
+
+    location.href = '/';
+});
