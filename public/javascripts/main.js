d5200a15ecae229c73ec04d3b4738a4076787152
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-01-16 19:40:24 +0100

    meu sql knex

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,9 +6,10 @@ function mostrarData() {
         ano: new Date().getFullYear(),
         hora: new Date().getHours(),
         minuto: new Date().getMinutes(),
+        second: new Date().getSeconds()
     }];
 
-    dateLocal.textContent = `${minhaData[0].dia}/${minhaData[0].mes}/${minhaData[0].ano} ${minhaData[0].hora}:${minhaData[0].minuto}`;
+    dateLocal.textContent = `${minhaData[0].dia}/${minhaData[0].mes}/${minhaData[0].ano} ${minhaData[0].hora}:${minhaData[0].minuto} ${minhaData[0].second}`;
 }
 
 mostrarData();
