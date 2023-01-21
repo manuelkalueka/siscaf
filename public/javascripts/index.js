3e4b85ba4af4b5232dc970f2409d60bf58051207
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-01-21 01:24:15 +0100

    Botões iniciais OK

---
+++ public/javascripts/index.js
@@ -0,0 +1,5 @@
+const btnLogin = document.querySelector('#btnLogin');
+btnLogin.addEventListener('click', () => location.href = '/login');
+
+const btnRegistar = document.querySelector('#btnRegistar');
+btnRegistar.addEventListener('click', () => location.href = '/registar');
